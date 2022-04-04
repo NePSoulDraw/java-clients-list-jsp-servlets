402c27b6c52cea93318e82b7d6ee1cdec2d5639a
Ismael Ahumada <nepsouldraw@gmail.com>
2022-04-04 20:28:49 +0200

    Improvements to SQL connection file

--- src/main/java/data/SqlConnection.java
+++ src/main/java/data/SqlConnection.java
@@ -13,15 +13,21 @@ public class SqlConnection {
     
     private static final String JDBC_PASSWORD = "admin";
     
+    private static BasicDataSource dataSource;
+    
     public static DataSource getDataSource(){
      
-        BasicDataSource ds = new BasicDataSource();
-        ds.setUrl(JDBC_URL);
-        ds.setUsername(JDBC_USER);
-        ds.setPassword(JDBC_PASSWORD);
-        ds.setInitialSize(50);
-        
-        return ds;
+        if(dataSource == null){
+            
+            dataSource = new BasicDataSource();
+            dataSource.setUrl(JDBC_URL);
+            dataSource.setUsername(JDBC_USER);
+            dataSource.setPassword(JDBC_PASSWORD);
+            dataSource.setInitialSize(50);
+            
+        }
+       
+        return dataSource;
         
     }
     
